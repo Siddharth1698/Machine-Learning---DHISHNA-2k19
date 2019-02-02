959a8d146ff7f78dee975146263913bbcc168506
Siddharth M <siddharth.m98@gmail.com>
2019-02-02 15:09:51 +0530

    labelencoding with transform

--- labelencoding.py
+++ labelencoding.py
@@ -5,6 +5,9 @@ from sklearn import preprocessing
 input_data = ["apple","orange","grapes"]
 label_encode = preprocessing.LabelEncoder()
 label_encode.fit(input_data)
-
+encoded_labels = label_encode.transform(input_data)
 for i,item in enumerate(label_encode.classes_): 
 	print (item, '->', i)
+
+print ("Labels =", input_data)
+print ("Encoded labels =", list (encoded_labels))
