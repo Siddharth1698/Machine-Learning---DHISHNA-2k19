8b0575653b7e4e178cd25af934560b6bd3d477c9
Siddharth M <siddharth.m98@gmail.com>
2019-02-02 11:46:37 +0530

    Basic Array

---
+++ ml1.py
@@ -0,0 +1,6 @@
+import numpy as np 
+from sklearn import preprocessing
+
+input_data = np.array([[1,2],[3,4]])
+print(input_data)
+
--- test.py
+++ test.py
@@ -1,1 +1,3 @@
-print ("Hello World!")
+print("Hello World!")
+x = 10
+print(x)
