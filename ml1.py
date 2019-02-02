4a82dc99d00db4314572752877f0dbb6bee05153
Siddharth M <siddharth.m98@gmail.com>
2019-02-02 11:50:33 +0530

    Mean of data

--- ml1.py
+++ ml1.py
@@ -1,6 +1,7 @@
 import numpy as np 
 from sklearn import preprocessing
 
-input_data = np.array([[1,2],[3,4]])
+input_data = np.array([[1,2],[3,4],[5,6]])
 print(input_data)
+print(input_data.mean(axis=0))
 
