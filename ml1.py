bfdc174cb05870918b3c8ed3d3df50a087827af2
Siddharth M <siddharth.m98@gmail.com>
2019-02-02 12:04:45 +0530

    Standard deviation and Scaling

--- ml1.py
+++ ml1.py
@@ -4,4 +4,7 @@ from sklearn import preprocessing
 input_data = np.array([[1,2],[3,4],[5,6]])
 print(input_data)
 print(input_data.mean(axis=0))
+print(input_data.std(axis=0))
+input_data_scaled = preprocessing.scale(input_data)
+print(input_data_scaled)
 
