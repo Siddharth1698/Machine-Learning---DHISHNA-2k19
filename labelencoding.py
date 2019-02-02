c5b3fedd6d8009073ae78a6ef729f7306722e854
Siddharth M <siddharth.m98@gmail.com>
2019-02-02 15:13:20 +0530

    label encoding with Inverse Transformation

--- labelencoding.py
+++ labelencoding.py
@@ -9,5 +9,9 @@ encoded_labels = label_encode.transform(input_data)
 for i,item in enumerate(label_encode.classes_): 
 	print (item, '->', i)
 
+decode_labels = label_encode.inverse_transform(encoded_labels)    
+
 print ("Labels =", input_data)
 print ("Encoded labels =", list (encoded_labels))
+print ("Encoded labels =", list (decode_labels))
+
