dafcc0bfd5ce4e23064f9d00225d0518515aad79
Siddharth M <siddharth.m98@gmail.com>
2019-02-03 10:35:02 +0530

    plotting petal and sepal data

--- svm.py
+++ svm.py
@@ -4,8 +4,11 @@ iris_flower = datasets.load_iris()
 shapr_iris = iris_flower.data.shape
 names = iris_flower.feature_names
 dataset = iris_flower.data
-X = iris_flower.data[:,:2]
+# X = iris_flower.data[:,:2]
+X = iris_flower.data[:,2:]
 y = iris_flower.target
 
 plt.scatter(X[:,0],X[:,1],c = y,cmap=plt.cm.coolwarm)
+
+
 plt.show()
