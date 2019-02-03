11a3c80e29dc3371363c1c85513c0d6b812af3da
Siddharth M <siddharth.m98@gmail.com>
2019-02-03 11:44:05 +0530

    Prediction result data

--- svm_reg.py
+++ svm_reg.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 iris_flower = datasets.load_iris()
 lin_svc = svm.SVC (kernel='linear',C=C)
-rbf_syn = svm.SVC (kernel='rbf',gamma=0.7,C=C)
+rbf_syn = svm.SVC (kernel='rbf',gamma=1.3,C=C)
 poly_svc = svm.SVC (kernel='poly',degree=3,C=C)
 
 X_train,X_test,y_train,y_test = train_test_split(iris_flower.data,iris_flower.target,test_size = 0.3)
@@ -14,7 +14,22 @@ lin_svc.fit(X_train,y_train)
 rbf_syn.fit(X_train,y_train)
 poly_svc.fit(X_train,y_train)
 
-print(X_train)
-print(y_train)
-print(X_test)
+# print(X_train)
+# print(y_train)
+# print(X_test)
+# print(y_test)
+
+lin_predict_result = lin_svc.predict(X_test)
 print(y_test)
+print(lin_predict_result)
+
+rbf_predict_result = rbf_syn.predict(X_test)
+print(y_test)
+print(rbf_predict_result)
+
+poly_predict_result = poly_svc.predict(X_test)
+print(y_test)
+print(poly_predict_result)
+
+
+
